e2fd951c16c178e9aac75a56d32414f0f5a85f24
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-01-04 22:51:46 +0530

    learn for in loop

--- loop.js
+++ loop.js
@@ -1,6 +1,6 @@
 // for loop
 for (let i = 0; i <= 10; i++) {
-  console.log(i);
+  // console.log(i);
 }
 
 // for of loop
@@ -8,4 +8,19 @@ let fruits = ["apple", "bannana", "mango"];
 
 for (let fruit of fruits) {
   console.log(fruit);
+}
+
+// for in loop
+const user = {
+  userName: "parvez",
+  password: 123456,
+  village: "basirhat",
+};
+
+for (let key in user) {
+  console.log(key);
+}
+
+for (let number = 0; number <= 12; number = number + 3) {
+  console.log(number);
 }
