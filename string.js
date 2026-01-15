ea93ad2b83736b794459f72899812ba98ad3115c
Parvez-31 <parveztarafder31@gmail.com>
2026-01-15 21:56:01 +0530

    String questions and methods, leapYear and evenOdd

--- string.js
+++ string.js
@@ -68,3 +68,48 @@ console.log(c3);
 
 // TOPIC: -> Template string
 console.log(`Hello world!! I am your beloved ${c1} and ${c2}`);
+
+// Method: -> Search method
+let h = "Let FIND the 'find' word in the text";
+let h1 = h.search(/find/gi);
+console.log(h);
+console.log(h1);
+
+// Method: -> indexOf method
+let h2 = h.indexOf("find");
+console.log(h2);
+
+let h3 = h.search("hello");
+console.log(h3);
+
+let h4 = h.indexOf("hello");
+console.log(h4);
+
+// Method: -> lastIndexOf
+let txt1 = "The world text in this text is very confusing text";
+let rtxt1 = txt1.lastIndexOf("text");
+console.log(txt1);
+console.log(rtxt1);
+
+// Method: -> startWith
+let txt2 = txt1.startsWith("The");
+let txt3 = txt1.startsWith("Hello");
+console.log(txt2);
+console.log(txt3);
+
+// Method: -> endsWith
+let txt4 = txt1.endsWith("rain");
+let txt5 = txt1.endsWith("text");
+console.log(txt4);
+console.log(txt5);
+
+// Method: -> includes
+let txt6 = txt1.includes("this");
+let txt7 = txt1.includes("you");
+console.log(txt6);
+console.log(txt7);
+
+// Method: -> split method
+let hello = "hello world";
+let sp = hello.split("*");
+console.log(sp);
