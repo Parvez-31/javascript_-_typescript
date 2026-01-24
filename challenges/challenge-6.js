790ecf39166e4c8c384a227d1e1183e79bdff358
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-01-24 23:54:50 +0530

    challenge-6, challenge-7

---
+++ challenges/challenge-6.js
@@ -0,0 +1,10 @@
+function findLargest(a, b, c) {
+  if (a >= b && a >= c) {
+    return a;
+  } else if (b >= a && b >= c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+console.log(findLargest(20, 40, 50));
