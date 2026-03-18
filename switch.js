8f0562ea8b2401ed36684c412f2bd1646210e294
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-03-18 14:31:45 +0530

    refactor: comment out switch statement and practice join method

--- switch.js
+++ switch.js
@@ -1,13 +1,15 @@
-switch (prompt("what is the weather like?")) {
-  case "rainy":
-    console.log("Remember to bring an umbrella.");
-    break;
-  case "sunny":
-    console.log("Dress lightly.");
-  case "cloudy":
-    console.log("Go outside.");
-    break;
-  default:
-    console.log("unknown weather type!");
-    break;
-}
+// switch (prompt("what is the weather like?")) {
+//   case "rainy":
+//     console.log("Remember to bring an umbrella.");
+//     break;
+//   case "sunny":
+//     console.log("Dress lightly.");
+//   case "cloudy":
+//     console.log("Go outside.");
+//     break;
+//   default:
+//     console.log("unknown weather type!");
+//     break;
+// }
+const arr = [1, 2, 3, 4, 5];
+console.log(arr.join());
